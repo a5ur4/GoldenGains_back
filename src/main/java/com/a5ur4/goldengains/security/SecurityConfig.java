8a17a664a5f13f61518d432816e0216a745060c7
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2025-04-06 23:31:25 -0300

    Enhance role-based access control in SecurityConfig; define permissions for users, categories, posts, news, music, lifts, and ranks endpoints.

--- src/main/java/com/a5ur4/goldengains/security/SecurityConfig.java
+++ src/main/java/com/a5ur4/goldengains/security/SecurityConfig.java
@@ -27,21 +27,41 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configure(http)) // Permite CORS
             .csrf(csrf -> csrf.disable()) // Desativa CSRF para APIs REST
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless API
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/login", "/auth/register").permitAll() // Acesso público
 
-                .requestMatchers("/users/**").permitAll()
-                .requestMatchers("/users/update_role/**").hasRole("ADMIN")
-            
+                // Permissões de acesso usuário
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/users/get_all", "/users/get_by_id/**", "/users/get_by_email/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/users/update/**", "/users/update_role/**", "/users/delete/**").hasRole("ADMIN")
+
+                // Permissões de acesso personal data
                 .requestMatchers("/personal-data/**").permitAll() 
-                .requestMatchers("/categories/**").permitAll() 
-                .requestMatchers("/posts/**").permitAll()
-                .requestMatchers("/news/**").permitAll()
-                .requestMatchers("/music/**").permitAll()
-                .requestMatchers("/lifts/**").permitAll()
-                .requestMatchers("/ranks/**").permitAll()
-                .anyRequest().authenticated() // Todas as outras rotas exigem autenticação
+
+                // Permissões de acesso categoria
+                .requestMatchers("/categories/get_all", "/categories/get_by_id/**").permitAll()
+                .requestMatchers("/categories/create", "/categories/update/**", "/categories/delete/**").hasAnyRole("USER", "ADMIN")
+                
+                // Permissões de acesso posts
+                .requestMatchers("/posts/get_all", "/posts/get_by_id/**").permitAll()
+                .requestMatchers("/posts/create", "/posts/update/**", "/posts/delete/**").hasAnyRole("USER", "ADMIN")
+
+                // Permissões de acesso news
+                .requestMatchers("/news/get_all", "/news/get_by_id/**").permitAll()
+                .requestMatchers("/news/create", "/news/update/**", "/news/delete/**").hasAnyRole("USER", "ADMIN")
+
+                // Permissões de acesso music
+                .requestMatchers("/music/get_all", "/music/get_by_id/**").permitAll()
+                .requestMatchers("/music/create", "/music/update/**", "/music/delete/**").hasAnyRole("USER", "ADMIN")
+
+                // Permissões de acesso lifts
+                .requestMatchers("/lifts/get_all", "/lifts/get_by_id/**").permitAll()
+                .requestMatchers("/lifts/create", "/lifts/update/**", "/lifts/delete/**").hasAnyRole("USER", "ADMIN")
+
+                // Permissões de acesso ranks
+                .requestMatchers("/ranks/get_all", "/ranks/get_by_id/**").permitAll()
+                .requestMatchers("/ranks/create", "/ranks/update/**", "/ranks/delete/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class); // Filtro JWT
 
